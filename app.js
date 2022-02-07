c4642b5b054b8a50cb3fecc0c7807b7fc2aab549
Christian <christianlockley4@gmail.com>
2022-02-07 15:57:37 -0800

    completed the counter js exercise

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
+const btns = document.querySelectorAll('.btn');
+const value = document.getElementById('value');
+let count = 0;
 
+btns.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+        if (event.target.classList.contains('decrease')) {
+            count -= 1;
+        } 
+        else if (event.target.classList.contains('increase')) {
+            count += 1;
+        }
+        else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = 'red';
+        }
+        else if (count < 0) {
+            value.style.color = 'green';
+        }
+        else {
+            value.style.color = 'black';
+        }
+        value.textContent = count;
+    });
+});
